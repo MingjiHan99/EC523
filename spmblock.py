83e28ee93b60f7f7e03723e5f6741d448dd3c687
Mingji <mjhan@bu.edu>
2023-03-29 01:58:03 +0000

    update fix typos

--- spmblock.py
+++ spmblock.py
@@ -1,24 +1,24 @@
 import torch.nn as nn
 
-class SPMSoftBlock(nn.Module):
+class SPDNormSoftBlock(nn.Module):
     def __init__(self):
-        super(SPMSoftBlock, self).__init__()
+        super(SPDNormSoftBlock, self).__init__()
         
     def forward(self):
         pass
     
 
-class SPMHardBlock(nn.Module):
+class SPDNormHardBlock(nn.Module):
     def __init__(self):
-        super(SPMHardBlock, self).__init__()
+        super(SPDNormHardBlock, self).__init__()
         
     def forward(self):
         pass
 
     
-class SPMResnetBlock(nn.Module):
+class SPDNormResnetBlock(nn.Module):
     def __init__(self):
-        super(SPMResnetBlock, self).__init__()
+        super(SPDNormResnetBlock, self).__init__()
         
     def forward(self):
         pass
