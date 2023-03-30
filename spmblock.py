a4b2f951a3dc3291d6b5283f0b9dfdc5fbd11bca
Mingji <mjhan@bu.edu>
2023-03-30 02:35:07 +0000

    update

--- spmblock.py
+++ spmblock.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 class SPDNormSoftBlock(nn.Module):
     def __init__(self):
-        super(SPMSoftBlock, self).__init__()
+        super(SPDNormSoftBlock, self).__init__()
         
     def forward(self):
         pass
@@ -10,7 +10,7 @@ class SPDNormSoftBlock(nn.Module):
 
 class SPDNormHardBlock(nn.Module):
     def __init__(self):
-        super(SPMHardBlock, self).__init__()
+        super(SPDNormSoftBlock, self).__init__()
         
     def forward(self):
         pass
@@ -18,7 +18,7 @@ class SPDNormHardBlock(nn.Module):
     
 class SPDNormResnetBlock(nn.Module):
     def __init__(self):
-        super(SPMResnetBlock, self).__init__()
+        super(SPDNormSoftBlock, self).__init__()
         
     def forward(self):
         pass
