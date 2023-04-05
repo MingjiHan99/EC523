3abae5c7aa2327374fe6f9ed457e2c024e93b0fd
Mingji <mjhan@bu.edu>
2023-04-05 20:31:55 +0000

    updates

--- spmblock.py
+++ spmblock.py
@@ -28,10 +28,31 @@ class SPDNormSoftBlock(nn.Module):
     
 
 class SPDNormHardBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channel, out_channel, k, mask_num):
         super(SPDNormHardBlock, self).__init__()
+        self.conv0 = nn.Conv2d(in_channel, in_channel, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3, padding=1)
+        self.mask_num = mask_num
         
     def forward(self):
+        img = F.interpolate(img, size=x.size()[2:])
+        mask = F.interpolate(mask, size=mask.size()[2:])
+        '''
+        pd_map = 0.0
+        for i in range(self.mask_number):
+            pd_map = self.mask_conv(mask_pre)
+            mask_generate = (mask_out - mask_pre) * (
+                1 / (torch.exp(torch.tensor(i + 1).cuda()))
+            )
+            mask_pre = mask_out
+            pd_map = pd_map + mask_generate
+        hard_map_inner = (1 - mask_out) * (1 / (torch.exp(torch.tensor(i + 1).cuda())))
+        hard_map = hard_map + mask_resize + hard_map_inner
+        hard_out = self.conv_0(self.actvn(self.norm_0(x, prior_image_resize, hard_map)))
+        hard_out = self.conv_1(
+            self.actvn(self.norm_1(hard_out, prior_image_resize, hard_map))
+        )
+        '''
         pass
 
     
@@ -41,8 +62,8 @@ class SPDNormResnetBlock(nn.Module):
         self.soft = SPDNormSoftBlock()
         self.hard0 = SPDNormHardBlock()
         self.hard1 = SPDNormHardBlock()
-        self.conv0 = nn.Conv2d(3, 128, kernel_size=3, padding=1)
-        self.conv1 = nn.Conv2d(3, 128, kernel_size=3, padding=1)
+        
+        
     def forward(self, x, img, mask):
         pass
     
